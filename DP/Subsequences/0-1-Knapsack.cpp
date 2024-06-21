723b0018184841f400fcb8d47713b589568f93a3
Siddhartha <siddharthapatwal@gmail.com>
2024-06-21 20:45:02 +0530

    removed comments

--- DP/Subsequences/0-1-Knapsack.cpp
+++ DP/Subsequences/0-1-Knapsack.cpp
@@ -23,83 +23,83 @@ class Solution
 {
     public:
     //Function to return max value that can be put in knapsack of capacity W.
-    // int helper(int i , int w ,int wt[], int val[], vector<vector<int>>&dp ){
-    //     //MEMOIZATION TC->O(N*W) SC->O(N*W+N)
-    //     if(i==0){
-    //         if(wt[0]<=w){
-    //             return val[0];
-    //         }
-    //         else return 0;
-    //     }
-    //     if(dp[i][w]!=-1){
-    //         return dp[i][w];
-    //     }
-    //     int notTake=0;
-    //     notTake= 0 + helper(i-1,w, wt,val,dp);
-    //     int take=INT_MIN;
-    //     if(wt[i]<=w){
-    //         take= val[i]+ helper(i-1, w-wt[i],wt,val,dp);
-    //     }
-    //     return dp[i][w]=max(notTake, take);
-    // }
-    // int knapSack(int W, int wt[], int val[], int n) 
-    // { 
-    //   // Your code here
-    // vector<vector<int>> dp(n, vector<int>(W+1, -1));
-    //   return helper(n-1, W , wt, val,dp);
+    int helper(int i , int w ,int wt[], int val[], vector<vector<int>>&dp ){
+        //MEMOIZATION TC->O(N*W) SC->O(N*W+N)
+        if(i==0){
+            if(wt[0]<=w){
+                return val[0];
+            }
+            else return 0;
+        }
+        if(dp[i][w]!=-1){
+            return dp[i][w];
+        }
+        int notTake=0;
+        notTake= 0 + helper(i-1,w, wt,val,dp);
+        int take=INT_MIN;
+        if(wt[i]<=w){
+            take= val[i]+ helper(i-1, w-wt[i],wt,val,dp);
+        }
+        return dp[i][w]=max(notTake, take);
+    }
+    int knapSack(int W, int wt[], int val[], int n) 
+    { 
+      // Your code here
+    vector<vector<int>> dp(n, vector<int>(W+1, -1));
+      return helper(n-1, W , wt, val,dp);
        
-    // }
+    }
 
 
     //Function to return max value that can be put in knapsack of capacity W.
-    // int knapSack(int W, int wt[], int val[], int n) 
-    // { 
-    //   //   tabulation tc-> O(N*W) SC->O(N*W)
-    //   vector<vector<int>> dp(n, vector<int>(W+1, -1));
-    //   for (int i=0;i<W+1;i++){
-    //       if(wt[0]<=i)dp[0][i]= val[0];
-    //       else dp[0][i]=0;
-    //   }
-    //   for(int i=1;i<n;i++){
-    //       for(int w=0;w<W+1;w++){
-    //           int take=INT_MIN;
-    //           if(wt[i]<=w){
-    //             take= val[i]+ dp[i-1][w-wt[i]];
-    //          }  
-    //          int notTake=0;
-    //          notTake= 0 + dp[i-1][w];
-    //          dp[i][w]=max(notTake, take);
-    //       }
-    //   }
-    //   return dp[n-1][W];
+    int knapSack(int W, int wt[], int val[], int n) 
+    { 
+      //   tabulation tc-> O(N*W) SC->O(N*W)
+      vector<vector<int>> dp(n, vector<int>(W+1, -1));
+      for (int i=0;i<W+1;i++){
+          if(wt[0]<=i)dp[0][i]= val[0];
+          else dp[0][i]=0;
+      }
+      for(int i=1;i<n;i++){
+          for(int w=0;w<W+1;w++){
+              int take=INT_MIN;
+              if(wt[i]<=w){
+                take= val[i]+ dp[i-1][w-wt[i]];
+             }  
+             int notTake=0;
+             notTake= 0 + dp[i-1][w];
+             dp[i][w]=max(notTake, take);
+          }
+      }
+      return dp[n-1][W];
        
-    // }
+    }
     
     
     //Function to return max value that can be put in knapsack of capacity W.
-    // int knapSack(int W, int wt[], int val[], int n) 
-    // { 
-    //   //   2-row space optimization tc-> O(N*W) SC->O(2*W)
-    //   vector<int> prev(W+1,0) , curr(W+1,0);
-    //   for (int i=0;i<W+1;i++){
-    //       if(wt[0]<=i)prev[i]= val[0];
-    //       else prev[i]=0;
-    //   }
-    //   for(int i=1;i<n;i++){
-    //       for(int w=0;w<W+1;w++){
-    //           int take=INT_MIN;
-    //           if(wt[i]<=w){
-    //             take= val[i]+ prev[w-wt[i]];
-    //          }  
-    //          int notTake=0;
-    //          notTake= 0 + prev[w];
-    //          curr[w]=max(notTake, take);
-    //       }
-    //       prev=curr;
-    //   }
-    //   return prev[W];
+    int knapSack(int W, int wt[], int val[], int n) 
+    { 
+      //   2-row space optimization tc-> O(N*W) SC->O(2*W)
+      vector<int> prev(W+1,0) , curr(W+1,0);
+      for (int i=0;i<W+1;i++){
+          if(wt[0]<=i)prev[i]= val[0];
+          else prev[i]=0;
+      }
+      for(int i=1;i<n;i++){
+          for(int w=0;w<W+1;w++){
+              int take=INT_MIN;
+              if(wt[i]<=w){
+                take= val[i]+ prev[w-wt[i]];
+             }  
+             int notTake=0;
+             notTake= 0 + prev[w];
+             curr[w]=max(notTake, take);
+          }
+          prev=curr;
+      }
+      return prev[W];
        
-    // }
+    }
     
      //Function to return max value that can be put in knapsack of capacity W.
     int knapSack(int W, int wt[], int val[], int n) 
